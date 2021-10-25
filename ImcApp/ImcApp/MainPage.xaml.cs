e9a6133db29b123ce2b4db468e7a0b86b27f3c31
angel17470085 <17470085@campeche.tecnm.mx>
2021-10-24 23:50:52 -0500

    agrega el manejo de eventos que da comportamiento a la interfaz de usuario de la aplicacion

--- ImcApp/ImcApp/MainPage.xaml.cs
+++ ImcApp/ImcApp/MainPage.xaml.cs
@@ -5,14 +5,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ImcApp.Model;
 
 namespace ImcApp
 {
     public partial class MainPage : ContentPage
     {
+       // private CalculadoraImc imc;
         public MainPage()
         {
             InitializeComponent();
+            LimpiarIU();
+        }
+
+        private void calcularButton_Clicked(object sender, EventArgs e)
+        {
+            decimal peso;
+            decimal estatura;
+            bool pesoEsConvertible = decimal.TryParse(pesoEntry.Text, out peso);
+            bool estaturaEsConvertible = decimal.TryParse(estaturaEntry.Text, out estatura);
+
+            if (pesoEsConvertible && estaturaEsConvertible)
+            {
+                CalculadoraImc cimc = new CalculadoraImc(peso, estatura);
+                imcLabel.Text = string.Format("{0:F4}", cimc.Imc);
+                situacionNutricionalLabel.Text = cimc.SituacionNutricional.ToString();
+            }
+
+            else
+            {
+                DisplayAlert(
+                    "Alerta",
+                    "El peso y la estatura deben ser valores numericos.",
+                    "Aceptar"
+                    );
+
+            }
+        }
+
+        private void limpiarButton_Clicked(object sender, EventArgs e)
+        {
+            LimpiarIU();
+        }
+
+        private void LimpiarIU()
+        {
+            pesoEntry.Text = string.Empty;
+            estaturaEntry.Text = string.Empty;
+            imcLabel.Text = string.Empty;
+            situacionNutricionalLabel.Text = string.Empty;
         }
     }
 }
