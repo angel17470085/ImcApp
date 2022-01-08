dc386fe140c94122f6b49e1fe2c0d9929994ed88
angel17470085 <17470085@campeche.tecnm.mx>
2022-01-08 14:56:06 -0600

    Aplicacion del Modelo Arquitectonico MVVM

---
+++ ImcApp/ImcApp/Models/CalculadoraImc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImcApp.Models
+{
+   public class CalculadoraImc
+    {
+       
+        //calcula el indice de masa corporal
+        public static decimal IndiceDeMasaCorporal(decimal peso, decimal estatura)
+        => peso / (estatura * estatura);
+
+        public enum EstadoNutricional
+        {
+            PesoBajo,
+            PesoNormal,
+            SobrePeso,
+            Obesidad,
+            ObesidadExtrema
+        }
+
+
+        public static EstadoNutricional SituacionNutricional(decimal imc)
+        {
+
+            if (imc < 18.5M)
+            {
+                return EstadoNutricional.PesoBajo;
+            }
+
+            else if (imc < 25.0M)
+            {
+                return EstadoNutricional.PesoNormal;
+            }
+
+            else if (imc < 30.0M)
+            {
+                return EstadoNutricional.SobrePeso;
+            }
+
+            else if (imc < 40.0M)
+            {
+                return EstadoNutricional.Obesidad;
+            }
+
+            else
+            {
+                return EstadoNutricional.ObesidadExtrema;
+            }
+        }
+
+     
+    }
+}
---
+++ ImcApp/ImcApp/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using ImcApp.ViewModels;
+
+namespace ImcApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+        }
+    }
+}
